72751f1d6b4219c540f58921428437bcbb04e9ec
nsandrez <n.orrego@outlook.cl>
2024-09-10 19:12:50 -0300

    Añade redirección basada en roles de usuario

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -25,7 +25,15 @@
                     <span class="text-xl font-bold">Tierra Animales</span>
                 </div>
                 <nav class="flex flex-col gap-2">
-                    <a href="#"
+                    <a href="
+                    @if (auth()->user()->rol == 'Administrador') {{ route('home.dashboard_admin') }}
+                    @elseif(auth()->user()->rol == 'Veterinario')
+                        {{ route('home.dashboard_vet') }}
+                    @elseif(auth()->user()->rol == 'Test')
+                        {{ route('home.dashboard_test') }}
+                    @else
+                        # @endif
+                    "
                         class="flex items-center gap-2 rounded-md px-4 py-2 text-lg font-medium transition-colors hover:bg-gray-100 hover:text-gray-900">
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
@@ -34,6 +42,7 @@
                         </svg>
                         Inicio
                     </a>
+
                     <a href="#"
                         class="flex items-center gap-2 rounded-md px-4 py-2 text-lg font-medium transition-colors hover:bg-gray-100 hover:text-gray-900">
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
